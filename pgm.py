64a1f1e79e54e7d4acd8fefc4eeba125c079251e
arathychandranl-cmd <arathychandranl@gmail.com>
2026-06-27 15:00:32 +0530

    Merge branch 'main' of https://github.com/mkn999/IMC-PROJEKT-FOOTBALL into football

--- pgm.py
+++ pgm.py
@@ -1,15 +1,17 @@
 from flask import Flask,jsonify
+from flask_cors import CORS
 app=Flask(__name__)
+CORS(app)
 @app.route('/data')
 def greet():
     a = {
         "name" : "Arathy",
         "age" : 16,
         "class": 10,
-        "Address":"Sreerangam venganoor",
-        "Roll_no" : 12,
-        "Register_no":123456,
+        "address":"Sreerangam venganoor",
+        "rnumber" : 12,
+        "registerNumber":123456,
     }
-    
+
     return jsonify(a)
 app.run(debug=True)
